22341c8152b6c2cdade43c1b6e139c166a98639b
aleksandrov.aleksey <aleksandrov.aleksey@otr.ru>
2016-02-24 11:05:40 +0300

    first histogram

--- script.js
+++ script.js
@@ -7,19 +7,15 @@ $import = function(src){
     document.getElementsByTagName("head")[0].appendChild(e);
 };
 
-Object.prototype.draw = function(){
-    console.log(this);
-};
-
 var chartData = function(labels, data, color){
     return {
-		labels : [labels],
+		labels : labels,
 		datasets : [
 			{
-				fillColor : "rgba(" + color + ")",
-				strokeColor : "rgba(220,220,220,0.8)",
-				highlightFill: "rgba(220,220,220,0.75)",
-				highlightStroke: "rgba(220,220,220,1)",
+				fillColor : "rgba(151,187,205,0.5)",
+				strokeColor : "rgba(151,187,205,0.8)",
+				highlightFill: "rgba(151,187,205,0.7)",
+				highlightStroke: "rgba(151,187,205,1)",
 				data : data
 			}
 		]
@@ -28,24 +24,30 @@ var chartData = function(labels, data, color){
 }
 
 Array.prototype.draw = function(){
-        var canvas = document.createElement("canvas");
-        var ctx = canvas.getContext("2d");
-    	document.getElementsByTagName("body")[0].appendChild(canvas);
-        switch (this[0].constructor.name){
-            case "Number":
-            case "String":
-            	window.myBar = new Chart(ctx).Bar(chartData(this, this, 100), {
-        	    	responsive : true
-            	});
-            	console.log("Number");
-                console.log("String");
-                break;
-            case "Object":;
-                console.log("Object");
-                break;
-            case "Array":;
-                console.log("Array");
-                break;
-        }
-        console.log(this);
-    };
+    var canvas = document.createElement("canvas");
+    var ctx = canvas.getContext("2d");
+  	document.getElementsByTagName("body")[0].appendChild(canvas);
+
+    switch (this[0].constructor.name){
+        case "Number":
+            var data = [];
+            var labels = [];
+            this.forEach(function(entry){
+               labels[entry-1] = entry + "";
+               data[entry-1] = data[entry-1] == undefined ? 1 : data[entry-1] + 1;
+            })
+        	window.myBar = new Chart(ctx).Bar(chartData(labels,data,100), {
+      	    	responsive : true
+           	});
+            break;
+        case "String":
+            break;
+        case "Object":;
+            console.log("Object");
+            break;
+        case "Array":;
+            console.log("Array");
+            break;
+    }
+    console.log(this);
+};
